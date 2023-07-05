1839d7dae3991b2b3672b04e9d510cd5c8fee24c
Arsyus <aidanxcannon@gmail.com>
2023-07-05 09:18:23 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import subprocess
+
+# Set the path to your local Git repository
+local_git_repository = "/path/to/your/local/git/repository"
+
+# Set the URL of your Azure App Service Git repository
+azure_git_repository = "https://<app-name>.scm.azurewebsites.net/<app-name>.git"
+
+# Change directory to your local Git repository
+subprocess.call(["cd", local_git_repository], shell=True)
+
+# Initialize the Git repository
+subprocess.call(["git", "init"], shell=True)
+
+# Add all files to the repository
+subprocess.call(["git", "add", "."], shell=True)
+
+# Commit the changes
+subprocess.call(["git", "commit", "-m", "Initial commit"], shell=True)
+
+# Set the remote repository URL
+subprocess.call(["git", "remote", "add", "azure", azure_git_repository], shell=True)
+
+# Push the code to Azure App Service
+subprocess.call(["git", "push", "azure", "master"], shell=True)
